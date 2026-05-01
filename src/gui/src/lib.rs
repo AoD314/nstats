17d105241be1237c4edaf391a38604a9206b453c
Andrey Morozov <morozov.andrey.vmk@gmail.com>
2026-05-01 21:54:17 +0300

    fix render

--- src/gui/src/lib.rs
+++ src/gui/src/lib.rs
@@ -1,23 +1,70 @@
 use ninjalib::ninjalib::NinjaFile;
 use sdl3::mouse::MouseButton;
-use sdl3::render::FPoint;
+use sdl3::render::{Canvas, FPoint};
+use sdl3::video::Window;
 use sdl3::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use sdl3_ttf_sys::ttf::{TTF_Font, TTF_TextEngine};
 use sdl3_ttf_sys::*;
 use std::time::{Duration, Instant};
 
 struct WindowSize {
-    x: u64,
-    y: u64,
+    x: u32,
+    y: u32,
     w: u32,
     h: u32,
-    block_h: u32,
-    block_delta: u32,
-    k: i32,
+
+    k: u8,           // ms in 100px
+    block_delta: u8, // space between lines in px
+    block_h: u16,    // height line in px
+}
+
+struct RectFile {
+    x: i32,
+    y: i32,
+    w: u32,
+    h: u32,
 }
 
 fn get_scale(ws: &WindowSize) -> f32 {
-    let base = 10.0f32;
-    base.powf(ws.k as f32 / 10.0)
+    100.0_f32 / (1 << ws.k) as f32
+}
+
+fn draw_cell(canvas: &mut Canvas<Window>, ws: &WindowSize, engine: *mut TTF_TextEngine, font: *mut TTF_Font) {
+    // Measuring scale
+
+    // set cell color
+    canvas.set_draw_color(Color::RGB(50, 50, 50));
+
+    let delta = ws.x % 100;
+    let count = ws.w / 100 + 1;
+    let step = 1u32 << ws.k;
+    let mut value: u32 = step * (ws.x / 100) as u32;
+
+    for i in 0..count {
+        let start = FPoint {
+            x: -(delta as f32) + i as f32 * 100.0,
+            y: 0.0,
+        };
+        let end = FPoint {
+            x: -(delta as f32) + i as f32 * 100.0,
+            y: ws.h as f32,
+        };
+        canvas.draw_line(start, end).unwrap();
+
+        let txt = format!("{:?} ms", value);
+        let text_len = txt.len();
+        let text = txt.as_ptr();
+
+        let x = 105.0_f32 - delta as f32 + i as f32 * 100.0;
+        let y = 16.0_f32;
+        value += step;
+
+        unsafe {
+            let text = ttf::TTF_CreateText(engine, font, text as *const i8, text_len);
+            ttf::TTF_DrawRendererText(text, x, y);
+            ttf::TTF_DestroyText(text);
+        }
+    }
 }
 
 pub fn run_window(ninja: NinjaFile) {
@@ -60,7 +107,7 @@ pub fn run_window(ninja: NinjaFile) {
         k: 1,
     };
 
-    let shift = 50i32;
+    let shift: i32 = 100;
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
@@ -93,12 +140,12 @@ pub fn run_window(ninja: NinjaFile) {
                         Some(Keycode::KpPlus) => {
                             win_size.block_h += 2;
                             win_size.block_h = if win_size.block_h > 64 { 64 } else { win_size.block_h };
-                            win_size.block_delta = win_size.block_h >> 1;
+                            win_size.block_delta = (win_size.block_h >> 1) as u8;
                         }
                         Some(Keycode::KpMinus) => {
                             win_size.block_h -= 2;
                             win_size.block_h = if win_size.block_h < 8 { 8 } else { win_size.block_h };
-                            win_size.block_delta = win_size.block_h >> 1;
+                            win_size.block_delta = (win_size.block_h >> 1) as u8;
                         }
                         Some(Keycode::Home) => {
                             win_size.x = 0;
@@ -130,17 +177,17 @@ pub fn run_window(ninja: NinjaFile) {
                         let shift_y = (pos_y as f32 - y) as i32;
 
                         if shift_x >= 0 {
-                            win_size.x = (pos_prev_x + shift_x) as u64;
+                            win_size.x = (pos_prev_x + shift_x) as u32;
                         } else {
                             let a = shift_x.abs() as i32;
-                            win_size.x = if pos_prev_x > a { (pos_prev_x - a) as u64 } else { 0 };
+                            win_size.x = if pos_prev_x > a { (pos_prev_x - a) as u32 } else { 0 };
                         }
 
                         if shift_y >= 0 {
-                            win_size.y = (pos_prev_y + shift_y) as u64;
+                            win_size.y = (pos_prev_y + shift_y) as u32;
                         } else {
                             let a = shift_y.abs() as i32;
-                            win_size.y = if pos_prev_y > a { (pos_prev_y - a) as u64 } else { 0 };
+                            win_size.y = if pos_prev_y > a { (pos_prev_y - a) as u32 } else { 0 };
                         }
                     }
                 }
@@ -151,10 +198,16 @@ pub fn run_window(ninja: NinjaFile) {
                 }
                 // Обработка прокрутки колесика
                 Event::MouseWheel { x: _, y, .. } => {
-                    win_size.k += y as i32;
+                    let mut v = win_size.k as i8;
+                    v += y as i8;
 
-                    win_size.k = if win_size.k > 20 { 20 } else { win_size.k };
-                    win_size.k = if win_size.k < -20 { -20 } else { win_size.k };
+                    if v < 0 {
+                        win_size.k = 0;
+                    } else if v >= 16 {
+                        win_size.k = 16;
+                    } else {
+                        win_size.k = v as u8;
+                    }
                 }
                 _ => {}
             }
@@ -164,49 +217,13 @@ pub fn run_window(ninja: NinjaFile) {
         canvas.set_draw_color(Color::RGB(10, 10, 10));
         canvas.clear();
 
-        // Measuring scale
-        // choose step
-        let scale_step = match scale {
-            0.01..0.1 => 1000,
-            0.1..1.0 => 100,
-            1.0..10.0 => 10,
-            _ => 1,
-        };
-
-        let mscale_color = match scale_step {
-            1 => Color::RGB(255, 0, 0),
-            10 => Color::RGB(0, 255, 0),
-            100 => Color::RGB(0, 0, 255),
-            1000 => Color::RGB(255, 255, 255),
-            _ => Color::RGB(50, 50, 50),
-        };
-
-        canvas.set_draw_color(mscale_color);
-
-        let mut index = 0;
-        loop {
-            let x = shift + ((index * scale_step) as f32 * scale) as i32 - win_size.x as i32;
-            if x > 0 {
-                let start = FPoint { x: x as f32, y: 0.0 };
-                let end = FPoint { x: x as f32, y: 45.0 };
-                canvas.draw_line(start, end).unwrap();
-            }
-            if x > win_size.w as i32 || index > 10000 {
-                break;
-            }
-            index += 1;
-
-            // println!("scale_step: {:?}", scale_step);
-            // println!("scale: {:?}", scale);
-            // println!("index: {:?}", index);
-            // println!("x: {:?}", x);
-        }
+        draw_cell(&mut canvas, &win_size, engine, font);
 
         canvas.set_draw_color(Color::RGB(255, 255, 255));
 
         for rec in ninja.records.iter() {
             let x = shift + (rec.start as f32 * scale) as i32 - win_size.x as i32;
-            let y = shift + (rec.thread_id * (win_size.block_h + win_size.block_delta)) as i32 - win_size.y as i32;
+            let y = shift + ((rec.thread_id) * (win_size.block_h as u32 + win_size.block_delta as u32)) as i32 - win_size.y as i32;
             let w = (rec.dur as f32 * scale) as u32;
             let h = (win_size.block_h) as u32;
 
